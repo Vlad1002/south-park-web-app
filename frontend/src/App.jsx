4d9478644e5c445c4f257fcadaab9a874b1af32e
Vlad Stoica <vladstoica102@gmail.com>
2025-11-09 10:36:08 +0200

    Add React Router with episode detail page  - Configure React Router with BrowserRouter  - Create EpisodeDetail component for individual episode pages  - Add navigation from list to detail page  - Implement back button to return to episodes list  - Optimize image display with responsive sizing  - Add View Details button on episode cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import EpisodeList from './components/EpisodeList'
+import EpisodeDetail from './components/EpisodeDetail'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <EpisodeList />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<EpisodeList />} />
+          <Route path="/episodes/:id" element={<EpisodeDetail />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
